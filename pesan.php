b057d1f99c1c5eb874eb513361244161dc5ae258
amrinarosada2712 <amrinaspd72@guru.smk.belajar.id>
2025-03-13 10:54:29 +0800

    versi 2

--- pesan.php
+++ pesan.php
@@ -58,6 +58,29 @@
             // Menghitung total pembayaran setelah dikurangi diskon dan ditambah biaya supir
             $total_bayar = $total_harga_mobil - $diskon + $biaya_supir;
         }
+        
+        // Jika tombol "Pesan" ditekan, tampilkan alert dan redirect
+    if (isset($_POST['simpan'])) {
+        $check = $supir == "checked" ? 'Ya' : 'Tidak';
+        $nama = $_POST['nama'];
+        $identitas = $_POST['identitas'];
+        $gender = $_POST['gender'];
+        $detail_pesanan = "Pesanan Berhasil!\n\n"
+            . "Nama: $nama\n"
+            . "Nomor Identitas: $identitas\n"
+            . "Jenis Kelamin: $gender\n"
+            . "Jenis Mobil: $pilihmobil \n"
+            . "Supir: $check\n" 
+            . "Durasi:$durasi \n"
+            . "Disokon: $diskon  \n"
+            . "Total Bayar: Rp " . number_format($total_bayar, 0, ',', '.');
+
+        echo "<script>
+            alert(`$detail_pesanan`);
+            window.location.href = 'index.php';
+        </script>";
+        exit();
+    }
     }
 ?>
 
@@ -132,7 +155,7 @@
                     <button type="submit" class="btn btn-primary">Hitung Total</button>
 
                     <!-- Tombol Simpan (akan mengarahkan ke hasil.php) -->
-                    <button type="submit" formaction="hasil.php" class="btn btn-primary">Simpan</button>
+                    <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
                     
                     <!-- Tombol Reset -->
                     <button type="reset" class="btn btn-danger">Cancel</button>
